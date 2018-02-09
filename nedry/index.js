0bc5ecb0c0828bd9c58121cd2441d739c39dcfc9
Tim Watkins <timothy.watkins@gmail.com>
2018-02-08 23:59:48 -0700

    Update index.js

--- nedry/index.js
+++ nedry/index.js
@@ -1,1 +1,26 @@
+// wrapper
+var elem = document.createElement('div');
+elem.style.cssText = 'position:absolute;width:100%;height:100%;';
+elem.id = "nedry";
 
+// img
+var img = document.createElement('img');
+img.src='https://raw.githubusercontent.com/jankcat/misc-scripts/master/nedry/nedry.gif';
+elem.appendChild(img);
+
+// img
+var audio = document.createElement('audio');
+audio.src='https://raw.githubusercontent.com/jankcat/misc-scripts/master/nedry/nedry.mp3';
+audio.autoplay='true';
+audio.loop='true';
+elem.appendChild(audio);
+
+document.body.appendChild(elem);
+
+(function(){
+  var e = document.getElementById('nedry');
+  var text = document.createElement('span');
+  text.innerHtml = "YOU DIDN'T SAY THE MAGIC WORD!";
+  e.appendChild(audio);
+  setTimeout(arguments.callee, 1000);
+})();
